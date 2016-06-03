7ece387235215e2c0ad1a4d37d63bddb5aff1cd8
Jeff Swenson <jeffongit@gmail.com>
2016-06-03 14:47:46 -0600

    init

---
+++ EndPointTest/EndPointTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
+
+namespace EndPointTest
+{
+	public class EndPointTest
+	{
+		private static TimeSpan defaultTimeSpan = new TimeSpan();
+
+		private TimeSpan timeOutSpan;
+		private string timeOut = "";
+		public string EndPoint { get; set; }
+
+		private HttpClient httpClient = null;
+
+		~EndPointTest()
+		{
+			Debug.WriteLine("~EndPointTest");
+			if (httpClient == null)
+			{
+				httpClient.Dispose();
+				httpClient = null;
+			}
+		}
+
+		public string TimeOut
+		{
+			get
+			{
+				return timeOut;
+			}
+			set
+			{
+				timeOut = value;
+				if (TimeSpan.TryParse(timeOut, out timeOutSpan) == false)
+				{
+					timeOutSpan = new TimeSpan();
+				}
+
+			}
+		}
+
+		public bool Test()
+		{
+			if (httpClient == null)
+			{
+				httpClient = new HttpClient { Timeout = timeOutSpan.CompareTo(defaultTimeSpan) > 0 ? timeOutSpan : new TimeSpan(0, 0, 0, 3, 0) };
+			}			
+			
+			try
+			{
+				var response = httpClient.GetAsync(this.EndPoint).Result;
+				response.EnsureSuccessStatusCode();
+			}
+			catch (Exception)
+			{
+				return false;
+			}
+
+			return true;
+		}
+	}
+}
---
+++ EndPointTest/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Net.Mime;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace EndPointTest
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var appSettings = ConfigurationManager.AppSettings;
+
+			IEnumerable<EndPointTest> items = null;
+			using (StreamReader r = new StreamReader("EndPointTests.json"))
+			{
+				var json = r.ReadToEnd();
+				items = JsonConvert.DeserializeObject<List<EndPointTest>>(json);
+			}
+
+			if (items != null)
+			{
+				var failures = new List<string>();
+				foreach (var item in items)
+				{
+					Debug.WriteLine(item.EndPoint);
+					if (item.Test() == false)
+					{
+						failures.Add(item.EndPoint);
+					}
+				}
+
+				if(failures.Any()) {
+					try
+					{
+						var mailMsg = new MailMessage();
+
+						// To
+						mailMsg.To.Add(new MailAddress(appSettings["toAddress"], appSettings["toName"]));
+
+						// From
+						mailMsg.From = new MailAddress(appSettings["fromAddress"], appSettings["fromName"]);
+
+						// Subject and multipart/alternative Body
+						mailMsg.Subject = "End Point Failures " + DateTime.Now.ToString(appSettings["dateFormat"]);
+						var buffer = new StringBuilder();
+						buffer.AppendLine("<h2>The following end points failed: </h2>");
+						buffer.AppendLine("<ul>");
+						failures.ForEach(i => buffer.AppendLine("<li>" + i + "</li>"));
+						buffer.AppendLine("<ul>");
+
+						mailMsg.AlternateViews.Add(AlternateView.CreateAlternateViewFromString(buffer.ToString(), null, MediaTypeNames.Text.Html));
+						
+						using (var smtpClient = new SmtpClient("smtp.sendgrid.net", Convert.ToInt32(587))) {
+							smtpClient.Credentials = new System.Net.NetworkCredential(appSettings["sendGridUser"], appSettings["sendGridPwd"]); ;
+							smtpClient.Send(mailMsg);
+						}
+						
+					}
+					catch (Exception ex)
+					{
+						Console.WriteLine(ex.Message);
+					}
+
+				}
+			}
+		}
+	}
+}
