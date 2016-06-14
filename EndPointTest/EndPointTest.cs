5f13df31acaf49d015948cb405ff8d26f6e36bef
Jeff Swenson <jeffongit@gmail.com>
2016-06-14 16:43:07 -0600

    Updates

--- EndPointTest/EndPointTest.cs
+++ EndPointTest/EndPointTest.cs
@@ -1,65 +1,183 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Net;
 using System.Net.Http;
+using System.ServiceProcess;
+using System.Text;
 
 namespace EndPointTest
 {
-	public class EndPointTest
-	{
-		private static TimeSpan defaultTimeSpan = new TimeSpan();
+    public class EndPointTest
+    {
+        private static TimeSpan defaultTimeSpan = new TimeSpan();
+        private static IList<string> RequestMethods = new List<string> { "GET", "POST" };
 
-		private TimeSpan timeOutSpan;
-		private string timeOut = "";
-		public string EndPoint { get; set; }
+        private TimeSpan timeOutSpan;
+        private string timeOut = "";
+        private string requestMethod = "GET";
+        private IList<KeyValuePair<string, string>> queryParameters = new List<KeyValuePair<string, string>>();
 
-		private HttpClient httpClient = null;
+        private HttpClient httpClient = null;
 
-		~EndPointTest()
-		{
-			Debug.WriteLine("~EndPointTest");
-			if (httpClient == null)
-			{
-				httpClient.Dispose();
-				httpClient = null;
-			}
-		}
+        ~EndPointTest()
+        {
+            Debug.WriteLine("~EndPointTest");
+            if (httpClient == null)
+            {
+                httpClient.Dispose();
+                httpClient = null;
+            }
+        }
 
-		public string TimeOut
-		{
-			get
-			{
-				return timeOut;
-			}
-			set
-			{
-				timeOut = value;
-				if (TimeSpan.TryParse(timeOut, out timeOutSpan) == false)
-				{
-					timeOutSpan = new TimeSpan();
-				}
 
-			}
-		}
+        public string EndPoint { get; set; }
 
-		public bool Test()
-		{
-			if (httpClient == null)
-			{
-				httpClient = new HttpClient { Timeout = timeOutSpan.CompareTo(defaultTimeSpan) > 0 ? timeOutSpan : new TimeSpan(0, 0, 0, 3, 0) };
-			}			
-			
-			try
-			{
-				var response = httpClient.GetAsync(this.EndPoint).Result;
-				response.EnsureSuccessStatusCode();
-			}
-			catch (Exception)
-			{
-				return false;
-			}
+        public string TimeOut
+        {
+            get
+            {
+                return timeOut;
+            }
+            set
+            {
+                timeOut = value;
+                if (TimeSpan.TryParse(timeOut, out timeOutSpan) == false)
+                {
+                    timeOutSpan = new TimeSpan();
+                }
 
-			return true;
-		}
-	}
+            }
+        }
+
+        public string RequestMethod
+        {
+            get
+            {
+                return requestMethod;
+            }
+            set
+            {
+                var tmpVal = value.ToUpper();
+                if (RequestMethods.Contains(tmpVal))
+                {
+                    requestMethod = tmpVal;
+                }
+                else
+                {
+                    requestMethod = "GET";
+                }
+            }
+        }
+
+        public IList<KeyValuePair<string, string>> QueryParameters
+        {
+            get
+            {
+                return queryParameters;
+            }
+            set
+            {
+                if (value != null)
+                    queryParameters = value;
+            }
+        }
+
+        public string NetworkUserName { get; set; }
+
+        public string NetworkPwd { get; set; }
+
+        public string ServiceName { get; set; }
+
+        public string ServiceOnMachineName { get; set; }
+
+        private string GetQueryString()
+        {
+            var result = new StringBuilder();
+            var delimiter = "?";
+            foreach (var item in QueryParameters)
+            {
+                result.Append($"{delimiter}{item.Key}={item.Value}");
+                delimiter = "&";
+            }
+
+            return result.ToString();
+        }
+
+        public bool Test()
+        {
+            if (httpClient == null)
+            {
+                if (string.IsNullOrWhiteSpace(NetworkUserName) || string.IsNullOrWhiteSpace(NetworkPwd))
+                {
+                    httpClient = new HttpClient { Timeout = timeOutSpan.CompareTo(defaultTimeSpan) > 0 ? timeOutSpan : new TimeSpan(0, 0, 0, 3, 0) };
+                }
+                else
+                {
+                    httpClient = new HttpClient(new HttpClientHandler { Credentials = new NetworkCredential(NetworkUserName, NetworkPwd) }) { Timeout = timeOutSpan.CompareTo(defaultTimeSpan) > 0 ? timeOutSpan : new TimeSpan(0, 0, 0, 3, 0) };
+                }
+            }
+
+            try
+            {
+                switch (RequestMethod)
+                {
+                    case "POST":
+                        break;
+                    case "GET":
+                    default:
+                        var request = $"{this.EndPoint}{GetQueryString()}";
+                        var response = httpClient.GetAsync(request).Result;
+                        response.EnsureSuccessStatusCode();
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool? StopService()
+        {
+            if (string.IsNullOrWhiteSpace(ServiceName))
+                return null;
+
+            ServiceController service = null;
+            try
+            {
+                service = new ServiceController(ServiceName, string.IsNullOrWhiteSpace(ServiceOnMachineName) ? ".": ServiceOnMachineName);
+                if (service.Status == ServiceControllerStatus.Stopped)
+                    return true;
+
+                service.Stop();
+                service.WaitForStatus(ServiceControllerStatus.Stopped);
+            }
+            catch (InvalidEnumArgumentException ex)
+            {
+                return null;
+                throw;
+            }
+            catch (ArgumentException ex)
+            {
+                return null;
+                throw;
+            }
+            catch (System.ServiceProcess.TimeoutException ex)
+            {
+                return service == null ? null : (bool?)(service.Status == ServiceControllerStatus.Stopped);
+                throw;
+            }
+            catch (Exception ex)
+            {
+                return null;
+                throw;
+            }
+
+            return service.Status == ServiceControllerStatus.Stopped;
+        }
+    }
 }
